69dddbaa5120fdb5b6f92d06c1e44746e7da0851
Henry Glick <henryglick@Henrys-MacBook-Pro.local>
2023-02-04 16:21:07 -0800

    Initial commit

---
+++ pa1.py
@@ -0,0 +1,15 @@
+# Name: pa1.py
+# Author(s):
+# Date:
+# Description:
+
+def gale_shapley(filename):
+    """
+    Runs Gale-Shapley algorithm on input
+    from file filename.  Format of input file
+    given in problem statement.
+    Returns a list containing hospitals assigned to each 
+    student, or None if a student is not assigned to a hospital.
+    """
+
+    pass
